b50b856e69dac5e777932f090967b7dc973a6cba
Mr-Vijay16 <hsvijay264737@gmail.com>
2026-05-20 15:01:44 +0530

    upadted seetings.py to deploy

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hf_cn_kr1*5q87c-+g1do$4gyfg10)%6wkhtkn-$@y5sx&me^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ STATIC_URL = 'static/'
 
 
 LOGIN_URL = "/login/"
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
